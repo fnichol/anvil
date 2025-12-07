3900f7a6c45b023343c50315e869a2dfb41c4fcc
Fletcher Nichol <fnichol@nichol.ca>
2025-12-07 11:46:43 -0700

    feat: create `anvil` with testing setup
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ lib/anvil/cli.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env sh
+# shellcheck disable=SC3043
+
+# Anvil CLI command parsing
+
+print_usage() {
+  local program="$1"
+  local version="$2"
+  local author="$3"
+
+  cat <<-EOF
+	$program $version
+
+	System provisioning and configuration management
+	
+	USAGE:
+	    $program [FLAGS] [COMMAND]
+	
+	FLAGS:
+	    -h, --help      Prints help information
+	    -V, --version   Prints version information
+	    -v, --verbose   Prints verbose output
+	
+	COMMANDS:
+	    apply           Converge system to desired state
+	    config          Manage configuration
+	    status          Show current vs desired state
+	    diff            Show what would change
+	    facts           Show discovered system info
+	    doctor          Verify system health
+	    list            List available tags or roles
+	    show            Show details of tag or role
+	EOF
+}
+
+anvil_cli() {
+  local program version author root
+  program="$1"
+  shift
+  version="$1"
+  shift
+  author="$1"
+  shift
+  root="$1"
+  shift
+
+  VERBOSE=""
+
+  OPTIND=1
+  while getopts "hvV-:" arg; do
+    case "$arg" in
+      h)
+        print_usage "$program" "$version" "$author"
+        return 0
+        ;;
+      v)
+        VERBOSE=true
+        ;;
+      V)
+        print_version "$program" "$version" "${VERBOSE:-}"
+        return 0
+        ;;
+      -)
+        # long_optarg="${OPTARG#*=}"
+        case "$OPTARG" in
+          help)
+            print_usage "$program" "$version" "$author"
+            return 0
+            ;;
+          verbose)
+            VERBOSE=true
+            ;;
+          version)
+            print_version "$program" "$version" "${VERBOSE:-}"
+            return 0
+            ;;
+          '')
+            # "--" terminates argument processing
+            break
+            ;;
+          *)
+            print_usage "$program" "$version" "$author" >&2
+            die "invalid argument --$OPTARG"
+            ;;
+        esac
+        ;;
+      \?)
+        print_usage "$program" "$version" "$author" >&2
+        die "invalid argument; arg=-$OPTARG"
+        ;;
+    esac
+  done
+  shift "$((OPTIND - 1))"
+
+  local command="${1:-}"
+  if [ -z "$command" ]; then
+    print_usage "$program" "$version" "$author"
+    return 0
+  fi
+  shift
+
+  case "$command" in
+    apply)
+      die "apply command not yet implemented"
+      ;;
+    config)
+      die "config command not yet implemented"
+      ;;
+    diff)
+      die "diff command not yet implemented"
+      ;;
+    doctor)
+      die "doctor command not yet implemented; root=$root"
+      ;;
+    facts)
+      die "facts command not yet implemented"
+      ;;
+    list)
+      die "list command not yet implemented"
+      ;;
+    show)
+      die "show command not yet implemented"
+      ;;
+    status)
+      die "status command not yet implemented"
+      ;;
+    *)
+      print_usage "$program" "$version" >&2
+      die "unknown command: $command"
+      ;;
+  esac
+}
---
+++ tests/test_helpers.sh
@@ -0,0 +1,248 @@
+#!/usr/bin/env sh
+# Test helper utilities for shUnit2
+
+# shellcheck disable=SC2034
+commonOneTimeSetUp() {
+  set -u
+
+  # Define root of project tree
+  if [ -n "${TEST_ROOT:-}" ]; then
+    root="$TEST_ROOT"
+  else
+    root="${0%/*}/.."
+  fi
+
+  __ORIG_FLAGS="$-"
+  __ORIG_PATH="$PATH"
+  __ORIG_TERM="$TERM"
+  __ORIG_PWD="$(pwd)"
+
+  tmppath="$SHUNIT_TMPDIR/tmp"
+
+  stdout="$tmppath/stdout"
+  stderr="$tmppath/stderr"
+  expected="$tmppath/expected"
+  actual="$tmppath/actual"
+  sh_script="$tmppath/sh_script.sh"
+  isolated_path="$tmppath/isolated_path"
+}
+
+commonSetUp() {
+  # Reset set flags to its original value
+  set "-$__ORIG_FLAGS"
+  # Reset the value of $PATH to its original value
+  PATH="$__ORIG_PATH"
+  # Reset the value of $TERM to its original value
+  TERM="$__ORIG_TERM"
+  # Restore the original working directory
+  cd "$__ORIG_PWD" || return 1
+  # Clean any prior test file/directory state
+  rm -rf "$tmppath"
+  # Unset any prior test variable state
+  unset return_status
+  # Create a scratch directory that will be removed on every test
+  mkdir -p "$tmppath"
+}
+
+assertStdoutEquals() {
+  if [ "$#" -eq 2 ]; then
+    assertEquals "$1" "$2" "$(cat "$stdout")"
+  else
+    assertEquals 'stdout not equal' "$1" "$(cat "$stdout")"
+  fi
+}
+
+assertStdoutStripAnsiEquals() {
+  if [ "$#" -eq 2 ]; then
+    assertEquals "$1" "$2" "$(stripAnsi <"$stdout")"
+  else
+    assertEquals 'stdout (strip ANSI) not equal' "$1" "$(stripAnsi <"$stdout")"
+  fi
+}
+
+assertStdoutContains() {
+  if [ "$#" -eq 2 ]; then
+    assertTrue "$1" "grep -E '$2' <'$stdout'"
+  else
+    assertTrue 'stdout does not contain' "grep -E '$1' <'$stdout'"
+  fi
+}
+
+assertStdoutStripAnsiContains() {
+  stripAnsi <"$stdout" >"$tmppath/stdout_no_ansi"
+
+  if [ "$#" -eq 2 ]; then
+    assertTrue "$1" "grep -E '$2' <'$tmppath/stdout_no_ansi'"
+  else
+    assertTrue 'stdout does not contain' "grep -E '$1' <'$tmppath/stdout_no_ansi'"
+  fi
+}
+
+assertStdoutNull() {
+  assertTrue 'stdout is not empty' "[ ! -s '$stdout' ]"
+}
+
+assertStderrEquals() {
+  if [ "$#" -eq 2 ]; then
+    assertEquals "$1" "$2" "$(cat "$stderr")"
+  else
+    assertEquals 'stderr not equal' "$1" "$(cat "$stderr")"
+  fi
+}
+
+assertStderrStripAnsiEquals() {
+  if [ "$#" -eq 2 ]; then
+    assertEquals "$1" "$2" "$(stripAnsi <"$stderr")"
+  else
+    assertEquals 'stderr (strip ANSI) not equal' "$1" "$(stripAnsi <"$stderr")"
+  fi
+}
+
+assertStderrContains() {
+  if [ "$#" -eq 2 ]; then
+    assertTrue "$1" "grep -E '$2' <'$stderr'"
+  else
+    assertTrue 'stderr does not contain' "grep -E '$1' <'$stderr'"
+  fi
+}
+
+assertStderrStripAnsiContains() {
+  stripAnsi <"$stderr" >"$tmppath/stderr_no_ansi"
+
+  if [ "$#" -eq 2 ]; then
+    assertTrue "$1" "grep -E '$2' <'$tmppath/stderr_no_ansi'"
+  else
+    assertTrue 'stderr does not contain' "grep -E '$1' <'$tmppath/stderr_no_ansi'"
+  fi
+}
+
+assertStderrNull() {
+  assertTrue 'stderr is not empty' "[ ! -s '$stderr' ]"
+}
+
+# Run command and capture stdout/stderr
+run() {
+  # Implementation inspired by `run` in bats
+  # See: https://git.io/fjCcr
+  _origFlags="$-"
+  set +e
+  # functrace is not supported by all shells, eg: dash
+  if set -o | "${GREP:-grep}" -q '^functrace'; then
+    # shellcheck disable=SC3041
+    set +T
+  fi
+  # errtrace is not supported by all shells, eg: ksh
+  if set -o | "${GREP:-grep}" -q '^errtrace'; then
+    # shellcheck disable=SC3041
+    set +E
+  fi
+  "$@" >"$stdout" 2>"$stderr"
+  return_status=$?
+  set "-$_origFlags"
+  unset _origFlags
+
+  return "$return_status"
+}
+
+__setup_sh_script() {
+  cat "${SRC:?SRC not set}" >"$sh_script"
+  cat "${0%/*}/_ksh_local.sh" >>"$sh_script"
+  echo >>"$sh_script"
+}
+
+run_in_sh_script() {
+  __setup_sh_script
+  while read -r line; do
+    echo "$line" >>"$sh_script"
+  done
+
+  run "${SHELL_BIN:-sh}" "$sh_script"
+}
+
+run_in_sh_script_and_signal() {
+  __setup_sh_script
+  while read -r line; do
+    echo "$line" >>"$sh_script"
+  done
+
+  echo "
+    # Run the script with the shell interpreter in the background
+    ${SHELL_BIN:-sh} $sh_script &
+    # Capture the pid of the script
+    bgps=\$!
+    # Sleep to wait for script to start running and to start writing to output
+    # streams
+    sleep 0.05
+    # Send the given signal to the script process
+    kill -s '$1' \$bgps
+    # Wait for the script process to terminate
+    wait \$bgps
+    # Return the exit code from the script process
+    exit $?
+  " >"$tmppath/run_in_bg.sh"
+
+  run "${SHELL_BIN:-sh}" "$tmppath/run_in_bg.sh"
+}
+
+run_with_sh_script() {
+  __setup_sh_script
+  echo '"$@"' >>"$sh_script"
+
+  run "${SHELL_BIN:-sh}" "$sh_script" "$@"
+}
+
+debugLastRun() {
+  echo "======================"
+  echo "Last 'run' invocation:"
+  echo "----------------------"
+  echo
+  echo "return_status=$return_status"
+  echo
+  echo "stdout:"
+  echo "---"
+  cat "$stdout"
+  echo "---"
+  echo
+  echo "stderr:"
+  echo "---"
+  cat "$stderr"
+  echo "---"
+  echo "======================"
+}
+
+stripAnsi() {
+  case "$(uname -s)" in
+    FreeBSD)
+      gsed -r 's,\x1B\[[0-9;]*[a-zA-Z],,g'
+      ;;
+    *)
+      # The `sed` implementation on macOS does not support either `\x1b` nor
+      # the `-r` flag, and dash has a bug
+      # (https://bugs.launchpad.net/ubuntu/+source/dash/+bug/1499473) where
+      # `\xNN` hex bytes can't be printed, therefore we'll emit the correct
+      # byte in octal with a `printf` subshell.
+      sed 's,'"$(printf "\033")"'\[[0-9;]*[a-zA-Z],,g'
+      ;;
+  esac
+}
+
+isolatedPathFor() {
+  mkdir -p "$isolated_path"
+
+  for _bin in "$@"; do
+    if command -v "$_bin" >/dev/null; then
+      ln -snf "$(command -v "$_bin")" "$isolated_path/$_bin"
+    fi
+  done
+}
+
+shell_compat() {
+  if [ -n "${ZSH_VERSION:-}" ]; then
+    # shellcheck disable=SC3040
+    set -o shwordsplit
+    SHUNIT_PARENT="$1"
+  fi
+}
+
+# shellcheck disable=SC2034
+shunit2="${0%/*}/../tmp/shunit2/shunit2"
