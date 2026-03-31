6e137a43283654241d0a5e375eab5a2ee84a1eff
Fletcher Nichol <fnichol@nichol.ca>
2026-03-30 20:21:49 -0600

    fix: type in Cider repo bootstrap hook
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- data/hooks/bootstrap/010-cider-repo.sh
+++ data/hooks/bootstrap/010-cider-repo.sh
@@ -18,8 +18,8 @@ bootstrap_hook_cider_repo() {
         download https://repo.cider.sh/ARCH-GPG-KEY "$gpg_key"
 
         info "Importing Cider Collective GPG key"
-        ident as_root pacman-key --add - <"$gpg_key"
-        ident as_root pacman-key --lsign-key A0CD6B993438E22634450CDD2A236C3F42A61682
+        indent as_root pacman-key --add - <"$gpg_key"
+        indent as_root pacman-key --lsign-key A0CD6B993438E22634450CDD2A236C3F42A61682
 
         info "Adding Cider Collective repository"
         as_root tee -a /etc/pacman.conf <<-'EOF' >/dev/null
