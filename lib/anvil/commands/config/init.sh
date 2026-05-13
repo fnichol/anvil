2539bb6a7f05893801fe4211bae2db50f8308661
Fletcher Nichol <fnichol@nichol.ca>
2026-05-12 18:55:16 -0600

    feat(init): `config init` supports add/install modules & calling apply
    This change introduces 3 new flags and options to the `anvil config
    init` subcommand:
    
    - `--module <MODULE>`: add the given module to the config file,
      mimicking the `anvil module add` subcommand. The same module string
      formats are supported as in the `anvil module add` subcommand and to
      set a custom name alias you can use the form of `--module
      alias::github.com/name/repo` to set the module alias to `alias`.
    - `--install`: installs all added modules which is not perfomed by
      default. Mimicks the `anvil module install` subcommand.
    - `--apply`: after initializing the setup, the flag invokes the `anvil
      apply` subcommand via the `exec` call.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- lib/anvil/commands/config/init.sh
+++ lib/anvil/commands/config/init.sh
@@ -5,10 +5,14 @@
 . "${SRC_ROOT}/lib/anvil/argparse.sh"
 # shellcheck source=lib/anvil/config.sh
 . "$SRC_ROOT/lib/anvil/config.sh"
+# shellcheck source=lib/anvil/modules.sh
+. "$SRC_ROOT/lib/anvil/modules.sh"
 
 print_usage_config_init() {
   local program="$1"
   local default_config_path="$2"
+  local default_data_home="$3"
+  local default_modules_lock_path="$4"
 
   cat <<-EOF
 	Initialize a config file
@@ -17,16 +21,21 @@ print_usage_config_init() {
 	    $program config init [FLAGS] [OPTIONS]
 	
 	FLAGS:
-	    -h, --help              Prints help information
+	    -a, --apply               Run apply after initializing
+	    -h, --help                Prints help information
+	    -i, --install             Install added modules
 
 	OPTIONS:
-	    -f, --fqdn=<FQDN>       Host FQDN (bare hostname will append .local
-	                            as FQDN)
-	    -r, --role=<R>[,<R>..]  Roles to use in config
-	    -t, --tag=<T>[,<T>..]   Tags to use in config
+	    -f, --fqdn=<FQDN>         Host FQDN (bare hostname will append
+	                              .local as FQDN)
+	    -m, --module=<M>[,<M>..]  Modules to add in config
+	    -r, --role=<R>[,<R>..]    Roles to use in config
+	    -t, --tag=<T>[,<T>..]     Tags to use in config
 
 	ENVIRONMENT VARIABLES:
-	    ANVIL_CONFIG_PATH       [default: $default_config_path]
+	    ANVIL_CONFIG_PATH         [default: $default_config_path]
+	    ANVIL_DATA_HOME           [default: $default_data_home]
+	    ANVIL_MODULES_LOCK_PATH   [default: $default_modules_lock_path]
 	EOF
 }
 
@@ -37,23 +46,40 @@ cmd_config_init() {
 
   local default_config_path config_file
   default_config_path="$(config_path)"
+  local default_data_home data_home
+  default_data_home="$(modules_data_home)"
+  local default_moduless_lock_path modules_lock_file
+  default_moduless_lock_path="$(modules_lock_path)"
 
   usage() {
     print_usage_config_init \
       "$program" \
-      "$default_config_path"
+      "$default_config_path" \
+      "$default_data_home" \
+      "$default_moduless_lock_path"
   }
 
+  local apply=""
+  local install=""
   local fqdn=""
+  local modules=""
   local roles=""
   local tags=""
 
   while [ $# -gt 0 ]; do
     case "$1" in
       # Flags
+      -a | --apply)
+        apply="true"
+        shift 1
+        ;;
       -h | --help)
         usage
         return 0
+        ;;
+      -i | --install)
+        install="true"
+        shift 1
         ;;
       # Options
       -f | --fqdn)
@@ -63,6 +89,15 @@ cmd_config_init() {
         ;;
       -f=?* | --fqdn=?*)
         fqdn="${1#*=}"
+        shift 1
+        ;;
+      -m | --module)
+        ensure_required_arg "$1" "${2:-}"
+        modules="${modules:+$modules,}$2" # modules are comma-delimited
+        shift 2
+        ;;
+      -m=?* | --module=?*)
+        modules="${modules:+$modules,}${1#*=}" # modules are comma-delimited
         shift 1
         ;;
       -r | --role)
@@ -108,9 +143,68 @@ cmd_config_init() {
   esac
 
   config_file="${ANVIL_CONFIG_PATH:-$default_config_path}"
+  data_home="${ANVIL_DATA_HOME:-$default_data_home}"
+  modules_lock_file="${ANVIL_MODULES_LOCK_PATH:-$default_moduless_lock_path}"
 
   config_create "$config_file" "$tags" "$roles" "$fqdn" \
     || die "Failed to init config"
 
   section "Created config file: $config_file"
+
+  local module
+  echo "$modules" | tr ',' ' ' | while read -r module; do
+    if [ -z "$module" ]; then
+      continue
+    fi
+
+    local name url
+    if echo "$module" | grep -q '::'; then
+      url="$(echo "$module" | awk -F '::' '{print $2}')"
+      # Normalize URL
+      url="$(module_expand_url "$url")"
+      name="$(echo "$module" | awk -F '::' '{print $1}')"
+    else
+      url="$module"
+      # Normalize URL
+      url="$(module_expand_url "$url")"
+      name="$(module_name_from_url "$url")"
+    fi
+
+    local mod_path
+    mod_path="$(module_path_for "$data_home" "$name")"
+
+    if [ -d "$mod_path" ]; then
+      die "Module directory $mod_path already exists; fix or delete & try again"
+    fi
+
+    section "Adding module $name"
+
+    module_config_add \
+      "$config_file" \
+      "$name" \
+      "$url" \
+      "" \
+      "" \
+      ""
+
+    if [ -n "$install" ]; then
+      module_install "$mod_path" "$url"
+
+      local current_sha
+      current_sha="$(git_current_sha "$mod_path")"
+
+      module_lock_update_for \
+        "$config_file" \
+        "$modules_lock_file" \
+        "$name" \
+        "$current_sha"
+    fi
+  done
+
+  if [ -n "$apply" ]; then
+    ANVIL_CONFIG_PATH="$config_file" \
+      ANVIL_DATA_HOME="$data_home" \
+      ANVIL_MODULES_LOCK_PATH="$modules_lock_file" \
+      exec "$0" apply
+  fi
 }
